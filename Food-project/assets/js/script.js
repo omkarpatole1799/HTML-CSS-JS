68201a098d9c0500e27a506621ea0889477ca726
Omkar Patole <omkarpatole1799@gmail.com>
2023-11-28 23:00:52 +0530

    Sticky navbar added using Js InsersectionObserver

--- Food-project/assets/js/script.js
+++ Food-project/assets/js/script.js
@@ -1,1 +1,28 @@
 console.log('script loaded')
+
+const heroSectionObserver = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0]
+
+    console.log(ent)
+    if (ent.isIntersecting) {
+      console.log('intersecting')
+      // ent.target.classList.add('sticky-nav')
+      // setTimeout(() => {}, 200)
+      document.querySelector('.section-header').classList.remove('sticky-nav')
+    } else {
+      console.log('not intersecting')
+      // ent.target.classList.remove('sticky-nav')
+      // setTimeout(() => {}, 200)
+      document.querySelector('.section-header').classList.add('sticky-nav')
+    }
+  },
+  {
+    root: null,
+    rootMargin: '-95px',
+    threshold: 0.2,
+  }
+)
+
+const heroSection = document.querySelector('.section-hero')
+heroSectionObserver.observe(heroSection)
