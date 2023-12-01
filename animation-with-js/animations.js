f81805081942728af2e77cb2331d627b9efb17e1
Omkar Patole <omkarpatole1799@gmail.com>
2023-12-01 07:54:01 +0530

    Add: top-slide-in and bottom-slide-in animations

--- animation-with-js/animations.js
+++ animation-with-js/animations.js
@@ -3,6 +3,9 @@ console.log('animations loaded')
 const rightSlideInEl = document.querySelectorAll('.right-slide-in')
 const leftSlideInEl = document.querySelectorAll('.left-slide-in')
 
+const bottomSlideInEl = document.querySelectorAll('.bottom-slide-in')
+const topSlideInEl = document.querySelectorAll('.top-slide-in')
+
 rightSlideInEl.forEach((el) => {
   el.style.opacity = '0'
   el.style.transform = 'translateX(50px)'
@@ -13,6 +16,18 @@ leftSlideInEl.forEach((el) => {
   el.style.opacity = '0'
   el.style.transform = 'translateX(-50px)'
   el.style.transition = 'all 0.35s ease-out'
+})
+
+bottomSlideInEl.forEach((el) => {
+  el.style.opacity = '0'
+  el.style.transform = 'translateY(50px)'
+  el.style.transition = 'all 0.5s ease-out'
+})
+
+topSlideInEl.forEach((el) => {
+  el.style.opacity = '0'
+  el.style.transform = 'translateY(-50px)'
+  el.style.transition = 'all 0.5s ease-out'
 })
 
 const slideElObserver = new IntersectionObserver(
@@ -44,4 +59,48 @@ rightSlideInEl.forEach((el) => {
 
 leftSlideInEl.forEach((el) => {
   slideElObserver.observe(el)
+})
+
+const bottomSlideElObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = '1'
+        entry.target.style.transform = 'translateY(0)'
+      } else {
+        entry.target.style.opacity = '0'
+        entry.target.style.transform = 'translateY(50px)'
+      }
+    })
+  },
+  {
+    root: null,
+    threshold: 0.25,
+  }
+)
+
+bottomSlideInEl.forEach((el) => {
+  bottomSlideElObserver.observe(el)
+})
+
+const topSlideElObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = '1'
+        entry.target.style.transform = 'translateY(0)'
+      } else {
+        entry.target.style.opacity = '0'
+        entry.target.style.transform = 'translateY(-50px)'
+      }
+    })
+  },
+  {
+    root: null,
+    threshold: 0.25,
+  }
+)
+
+topSlideInEl.forEach((el) => {
+  topSlideElObserver.observe(el)
 })
