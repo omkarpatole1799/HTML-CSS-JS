b3f14d29a4f7af49641987d59d05b0834666587f
Omkar Patole <omkarpatole1799@gmail.com>
2024-04-17 08:14:22 +0530

    Formatting

--- text-editor/index.js
+++ text-editor/index.js
@@ -1,44 +1,36 @@
-window.addEventListener('DOMContentLoaded',()=>{
-	let boldBtn = document.querySelector('#bold-btn')
-	let editor = document.querySelector('.editor')
-	let text ='' 
+window.addEventListener("DOMContentLoaded", () => {
+  let boldBtn = document.querySelector("#bold-btn")
+  let editor = document.querySelector(".editor")
+  let text = ""
 
-	let selectionStart
-	let selectionEnd
+  let selectionStart
+  let selectionEnd
 
-	editor.addEventListener('keyup', function(){
-		text = editor.innerHTML
-		if (text.length == 1) {
-			text = `<p>${text}</p>`
-		} else {
-			text = text
-		}
-		
-		console.log(text)
-		renderText(text)
-	})
+  editor.addEventListener("keyup", function () {
+    text = editor.innerHTML
+    renderText(text)
+  })
 
-	boldBtn.addEventListener('click', function(event){
-		let selection = window.getSelection()
-		selectionStart = selection.anchorOffset
-		selectionEnd = selection.focusOffset
-		console.log(selection)
-		console.log(selectionStart)
-		console.log(selectionEnd)
+  boldBtn.addEventListener("click", function (event) {
+    let selection = window.getSelection()
+    selectionStart = selection.anchorOffset
+    selectionEnd = selection.focusOffset
+    makeSelectionBold(selectionStart, selectionEnd)
+  })
 
-		insertString(selectionStart,selectionEnd)
-	})
+  function makeSelectionBold(start, end) {
+    text =
+      text.slice(0, start) +
+      `<strong>` +
+      text.slice(start, end) +
+      "</strong>" +
+      text.slice(end)
 
-	function insertString(start, end){
-		text = text.slice(0, start) + `<strong>`+ text.slice(end) 
-		text = text.slice(0, end) + `</strong>` + text.slice(end)
+    renderText(text)
+  }
 
-		renderText(text)
-	}
-
-	function renderText(text) {
-		editor.innerHTML = text
-		// editor.insertAdjacentElement("beforeend",text)
-	}
-
+  function renderText(text) {
+    text = "<p>" + text + "</p>"
+    console.log(text)
+  }
 })
