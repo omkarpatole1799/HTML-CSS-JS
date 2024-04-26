7ed25b81a31192ca277eaca848722539406870c0
Omkar Patole <omkarpatole1799@gmail.com>
2024-04-26 19:38:02 +0530

    Refactoring

--- attendance-managent/application/controllers/staffController.js
+++ attendance-managent/application/controllers/staffController.js
@@ -2,6 +2,8 @@ const staffModel = require("../models/staffModel.js")
 const staffController = {
   getStaffLoginPage: (req, res, next) => {
     try {
+      if (req.session.userName && req.session.password)
+        return res.redirect("/staff/dashboard")
       res.render("staff/staff-login.ejs")
     } catch (error) {
       console.log(error)
@@ -67,7 +69,6 @@ const staffController = {
     try {
       let department = req.query.department
       let year = req.query.year
-      console.log(department, year)
       let _subListRes = await staffModel.getSubList(department, year)
       return res.status(200).json({
         success: true,
@@ -83,7 +84,6 @@ const staffController = {
     try {
       let data = req.body
       let _addSubResponse = await staffModel.addSubject(data)
-      console.log(_addSubResponse, "-response")
       if (_addSubResponse[0].affectedRows === 1) {
         return res.status(201).json({
           success: true,
@@ -99,7 +99,6 @@ const staffController = {
   deleteSubject: async (req, res, next) => {
     try {
       let _deleteSubRes = await staffModel.deleteSubject(req.body.deleteId)
-      console.log(_deleteSubRes)
       if (_deleteSubRes[0].affectedRows === 1) {
         return res.status(201).json({
           success: true,
@@ -117,8 +116,6 @@ const staffController = {
     try {
       let _staffLoginDetails = await staffModel.loginStaff(req.body)
 
-      console.log(_staffLoginDetails)
-
       if (_staffLoginDetails[0].length == 0) {
         return res.status(401).json({
           success: false,
@@ -129,8 +126,6 @@ const staffController = {
       if (_staffLoginDetails[0].length >= 1) {
         req.session.userName = _staffLoginDetails[0][0].username
         req.session.password = _staffLoginDetails[0][0].password
-
-        console.log(req.session, "-session ")
         return res.status(200).json({
           success: true,
           status: 200,
