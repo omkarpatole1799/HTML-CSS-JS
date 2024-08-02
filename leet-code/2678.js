e31516cd45714f375e32c67f633a24062a3a73b1
omkarpatole1799 <omkarpatole1799@gmail.com>
2024-08-02 08:01:00 +0530

    added sol 2

--- leet-code/2678.js
+++ leet-code/2678.js
@@ -2,14 +2,24 @@ const details = ["7868190130M7522", "5303914400F9211", "9273338290F4010"];
 const detials2 = ["1313579440F2036", "2921522980M5644"];
 
 function checkMoreThanSixty(list) {
-  let returnList = [];
+  // sol 1
+  // let returnList = [];
+  // list.forEach(el => {
+  //   let age = el.slice(11, 13);
+  //   if (age > 60) {
+  //     returnList.push(age);
+  //   }
+  // });
+  // return returnList.length;
+
+  // sol2
+  let count = 0;
   list.forEach(el => {
-    let age = el.slice(11, 13);
-    if (age > 60) {
-      returnList.push(age);
-    }
+    const age = el.slice(11, 13);
+    if (age > 60) count += 1;
   });
-  return returnList.length;
+
+  return count;
 }
 
 console.log(checkMoreThanSixty(details));
