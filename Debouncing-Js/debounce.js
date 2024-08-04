3efb9cab27ca4a44b76962f6f6d08c5334cb196a
omkarpatole1799 <omkarpatole1799@gmail.com>
2024-08-04 21:55:50 +0530

    Add: debouncing sample example

---
+++ Debouncing-Js/debounce.js
@@ -0,0 +1,15 @@
+function searchData(e) {
+  console.log("===");
+}
+
+function debounceChange(callFn, delay) {
+  let timer = null;
+  return e => {
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      callFn(e);
+    }, delay);
+  };
+}
+
+const fn = debounceChange(searchData, 200);
